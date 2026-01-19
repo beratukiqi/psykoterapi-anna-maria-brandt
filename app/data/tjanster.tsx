a0a2f441abacfbbd492d46ec49e9bb13c0454d4a
beratukiqi <berat.u@live.se>
2026-01-19 15:41:55 +0100

    Parterapi + om mig text

--- app/data/tjanster.tsx
+++ app/data/tjanster.tsx
@@ -52,18 +52,36 @@ export const tjanster: Service[] = [
   {
     slug: "parterapi",
     name: "Parterapi",
-    title: "Behöver du reparera din relation med din partner?",
+    title: "Vårt mående påverkas starkt av hur vi upplever våra relationer.",
     image: "/images/parterapi2.jpg",
-    description: "Livets glädje uppstår i relationer och det gör också dess problem och svårigheter",
-    content: <><p>Du kan ha upplevt dig sviken. Eller så känner du att din partner inte längre förstår dig och dina livsdrömmar. <br /><br />
-      Kanske har du svårt att skapa en bra kontakt med dina barn. Relationen till din expartner, tillika barnens andre förälder, dina egna föräldrar, eller svärföräldrar kan vara krånglig. Du kanske känner dig utmattad eller orolig över att saker aldrig kommer att förändras. Oavsett vad du upplever, märker du hur detta påverkar ditt liv om det är:</p>
+    description: "Genom parterapi kan ni båda få en ökad ömsesidig förståelse, vilket kan hjälpa er att komma närmare varandra och bidra till ett ökat välbefinnande.",
+    content: <div className="flex flex-col gap-6">
+      <p className="font-semibold">
+        Genom parterapi kan ni båda få en ökad ömsesidig förståelse, vilket kan hjälpa er att komma närmare varandra och bidra till ett ökat välbefinnande.
+      </p>
+      <p>
+        Har du svårt att släppa på kontrollen i kärleksrelationer? Undviker du att visa intima känslor av rädsla för att bli sårad eller övergiven? Känner du dig ibland klängig eller krävande, för att senare hota med att avsluta relationen när du känner dig avvisad?
+      </p>
 
-      <ul className="list-disc ml-6 mt-8">
-        <li>ständiga bråk och oenigheter, även över små saker</li>
-        <li>känslor av spänning, frustration och uppgivenhet</li>
-        <li>oförmåga att komma över tidigare oförrätter eller lösa ångerfulla händelser</li>
-      </ul>
-    </>,
+      <p>
+        När våra känslomässiga behov inte blir tillgodosedda, eller när vi upplever hot om övergivenhet, aktiveras vårt stressystem. Detta kan leda till frustration, ilska eller undvikande beteenden som är avsedda att skydda oss. Om du kommer från otrygga uppväxtförhållanden eller har negativa erfarenheter av nära relationer kan det vara extra utmanande att fungera i ett parförhållande. Samtidigt strävar de flesta av oss efter att hitta en trygg kärleksrelation där vi kan slappna av och känna oss viktiga, vilket kan kännas svårt när vi har lärt oss att vara självständiga och undvika att luta oss på andra.
+      </p>
+
+      <h2 className="text-3xl font-medium mb-4 mt-12">
+        Att få knyta an
+      </h2>
+      <p className="font-semibold italic">
+        "Under hela vuxenlivet är tillgängligheten till en tillmötesgående anknytningsperson källan till en persons trygghetskänsla. Alla vi människor, från vaggan till graven, är lyckligast när livet organiseras som en serie utflykter, långa eller korta, från den trygga hamn som vi finner i våra anknytningspersoner."
+        <span className="block mt-4">- John Bowlby</span>
+      </p>
+      <p>Sund närhet till personer som vill oss väl skapar både känslomässigt och fysiskt välbefinnande och lägger grunden för inre trygghet. En förutsägbar kontakt med en annan människa eller partner erbjuder en stabil grund och inre lugn, vilket ger oss möjlighet att utforska världen eller utvecklas inom andra områden i livet.</p>
+      <p>En kärleksrelation handlar om att erkänna viljan att luta sig mot varandra, att finnas där för varandra när det är tufft och att öppna sig för intima känslor. På så sätt får vi möjlighet att uppleva trygga relationer präglade av värme och ömsesidighet – ett "sunt beroende". Visst finns det en risk att bli sårad eller övergiven, men den risken är nödvändig för att få uppleva glädjen av att vara nära en annan människa.
+      </p>
+
+      <h2 className="text-3xl font-medium mb-4 mt-12">Parterapi en väg till förändring</h2>
+      <p>EFT (Emotionally Focused Therapy för par) är en metod med stark forskningsbas som grundar sig i anknytnings-, humanistisk och systemisk teori. Metoden utgår från att våra känslor beskriver våra behov, värderingar och mål, både i relation till oss själva och inom kärleksrelationen. I EFT ses emotionerna som nyckeln till förändring. Istället för att stänga av eller fly från känslor—i syfte att skydda oss från känslomässig smärta—behöver vi öppna oss och både visa och uttrycka känslor. Känslorna fungerar som ett navigeringssystem för att tillgodose våra själsliga behov. När vi i vår omgivning inte får våra behov tillfredsställda blir vi frustrerade, ledsna och olyckliga. Genom att medvetandegöra dessa behov får vi viktiga insikter och nya möjligheter till att skapa tillfredsställande relationer.
+      </p>
+    </div>,
     ctaTitle: "Boka kostnadsfritt första samtal",
     ctaLink: "https://www.bokadirekt.se/boka-tjanst/psykoterapimottagning-anna-maria-brandt-47408/privat-parterapi-1859192",
   },
--- app/om-mig/page.tsx
+++ app/om-mig/page.tsx
@@ -15,27 +15,17 @@ export default function OmMig() {
 
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
               <img className="rounded-3xl order-1 lg:order-2" src="https://annamariabrandt.se/wp-content/uploads/2023/05/anna-maria-brandt.jpg" alt="" />
-              <div className="order-2 lg:order-1">
+              <div className="order-2 lg:order-1 flex flex-col gap-6">
                 <p className="font-medium">
-                  Att arbeta som psykoterapeut är för mig mer än ett yrke - det är ett högst medvetet livsval. Sedan jag fick min legitimation 2009 har jag valt att ägna mitt arbete åt att stötta en särskilt utsatt målgrupp, människor som ofta kämpar i det tysta och bär på komplexa sår från relationer präglade av trauma och manipulation. Jag arbetar med både individer och par och har sett hur samtal kan skapa ökad klarhet, livskvalitet och personlig utveckling. Ibland sker detta gemensamt i en parrelation, och ibland behöver det ske individuellt, där varje person får tid och utrymme att bearbeta på sitt eget sätt.
+                  Jag har många års erfarenhet av att arbeta med psykologisk och psykoterapeutisk behandling, både genom kortare och längre insatser som är fokuserade på att skapa djupgående verklig förändring. Mitt arbete grundar sig på evidensbaserade och beprövade psykologiska metoder som har visat sig vara effektiva.
                 </p>
-
-                <br />
-                <br />
-
-                <p>Under mina verksamma år som terapeut har jag mött hundratals klienter som kämpar med effekterna av destruktiva relationer, särskilt de som präglats av narcissism och emotionella övergrepp. Jag vet av både professionell erfarenhet och personlig insikt hur förvirrande och förlamande en sådan relation kan vara. Gaslighting och manipulation kan leda till att man börjar tvivla på sin egen verklighetsuppfattning och till sist inte vet varken ut eller in. I sådana situationer behövs både skräddarsydda verktyg och tydliga, handfasta råd för att kunna återfå sin inre kompass och börja bygga en stabil grund för framtiden.</p>
+                <p>I mitt arbete möter jag ofta olika typer av relationsproblem, personlighetsutmaningar, sviktande självkänsla, ångest och depressiva känslor, som ofta kan förekomma samtidigt. Det har visat sig vara både meningsfullt och framgångsrikt att integrera olika behandlingsmetoder, vilket ger oss möjlighet att arbeta med både symtom och de inre svårigheter som kan påverka dig.
+                </p>
+                <p className="font-semibold"> Vi anpassar behandlingen efter dina behov.
+                </p>
               </div>
             </div>
 
-
-            <p>
-              En av de stora fördelarna med att söka professionellt stöd är att du får hjälp från någon som står utanför dina egna nätverk och relationer. Jag vill ge en objektiv och opartisk reflektion som inte påverkas av förutfattade meningar eller känslomässiga band. I våra samtal får du möjlighet att reflektera över dina tankar och känslor i en trygg och förtrolig miljö, fri från dömande perspektiv. Här kan du utforska det du går igenom och få hjälp att sätta ord på sådant som kan vara svårt att dela med andra
-            </p>
-
-            <p>På min mottagning på Övre Slottsgatan fokuserar vi på att lindra den smärta du bär på, påbörja läkningsprocessen, små steg för att bygga en stabil grund för framtiden. Jag arbetar med beprövade metoder och strategier som hjälper dig att hantera den situation du står inför, alltid med målet att stärka din egen förmåga att navigera ditt liv framåt.</p>
-            <p className="font-medium">
-              Om du känner att det är dags att ta hjälp för att få klarhet eller stöd i din situation, erbjuder jag ett kostnadsfritt första samtal. Det är ett tillfälle för oss att lära känna varandra och utforska hur jag kan hjälpa dig att komma vidare. Att söka hjälp är ett modigt steg och kan bli början på en viktig förändring i ditt liv.
-            </p>
           </div>
         </section>
 
